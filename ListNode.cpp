805ad8c50ca802fe80805225b9d165e4bb78e564
Gabriel0402 <zhuch@seas.upenn.edu>
2015-04-02 23:34:04 -0700

    1 file submitted

--- ListNode.cpp
+++ ListNode.cpp
@@ -46,6 +46,25 @@ ListNode *reverseBetween(ListNode *head, int m, int n){
 	return dummy.next;
 }
 
+ListNode *partition(ListNode* head, int x){
+	ListNode left_dummy(-1);
+	ListNode right_dummy(-1);
+	auto left_cur = &left_dummy;
+	auto right_cur = &right_dummy;
+	for(ListNode *cur = head; cur; cur=cur->next){
+		if(cur->val <x{
+			left_cur->next = cur;
+			left_cur = cur;
+		})else{
+			right_cur ->next= cur;
+			right_cur = cur;
+		}
+		left_cur->next= right_dummy.next;
+		rig-t_cur ->next=null;
+		return left_dummy.next;
+	}
+}
+
 int main(int argc, char *argv[]) {
 	
 }
