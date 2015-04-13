def2df0dfe304312b396cca30a8224f5dce0b588
Gabriel0402 <zhuch@seas.upenn.edu>
2015-04-12 22:05:33 -0700

    new files submiteed

---
+++ largestRectangleArea.cpp
@@ -0,0 +1,16 @@
+int largestRectangleArea(vector<int> &height){
+	stack<int> s;
+	height.push_back(0);
+	int result=0;
+	for(int i=0;i<height.size();i++){
+		if(s.empty() || height[i]>s.top()){
+			s.push(i++);
+		} 
+		else{
+			int tmp=s.top();
+			s.pop();
+			result=max(result,height[tmp]*(s.empty()?i:i-s.top()-1));
+		}
+	}
+	return result;
+}
