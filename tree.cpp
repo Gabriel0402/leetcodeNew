70de863f0885f112a30499cd0801e18b02ab73c9
Gabriel0402 <zhuch@seas.upenn.edu>
2015-04-20 23:37:34 -0700

    new file submitted

--- tree.cpp
+++ tree.cpp
@@ -305,3 +305,22 @@ void pathSum(TreeNode *root, int gap, vector<int> &cur, vector<vector<int>> &res
 	pathSum(root->right,gap-root->val,cur,result);
 	cur.pop_back();
 }
+
+
+// time : o(n) space:o(logn)
+int maxPathSum(TreeNode *root){
+	max_sum=INT_MIN;
+	dfs(root);
+	return max_sum;
+}
+int max_sum;
+int dfs(const TreeNode *root){
+	if(root==NULL) return 0;
+	int l=dfs(root->left);
+	int r=dfs(root->right);
+	int sum=root->val;
+	if(l>0) sum+=l;
+	if(r>0) sum+=r;
+	max_sum=max(max_sum,sum);
+	return max(r,l)>0?max(r,l)+root->val:root->val;
+}
